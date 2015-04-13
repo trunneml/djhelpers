9f133e01116e9a793800c52b20b94b575e86f1bb
Michael Trunner <michael@trunner.de>
2015-04-13 18:35:13 +0200

    Some pylint corrections

--- djhelpers/adminhelpers.py
+++ djhelpers/adminhelpers.py
@@ -1,32 +1,35 @@
 # Copyright 2014 Michael Trunner
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from djhelpers import modelhelpers
+"""
+Some django admin helper classes and decorators.
+"""
 
-short_description = modelhelpers.short_description
+# The shot_description decorator can be helpful in the django admin, too.
+from djhelpers.modelhelpers import short_description  # pylint: disable=unused-import
 
 
 class ActionDecorator(list):
     """
     This class is a easy way to create a list of django admin actions.
-    
+
     Just create a actions field of that class in our model admin and
     write @actions.action('Nice description') on top of every admin
     action method in your model admin class.
-    
+
     Usage:
-    
+
     >>> from django.contrib import admin
     >>>
     >>> class SomeModelAdmin(admin.ModelAdmin):
@@ -39,25 +42,25 @@ class ActionDecorator(list):
     ...
     >>>
     """
-    
+
     def action(self, description):
         """
         Adds a short_description field to the decorated method and
         adds it to the (admin actions) list.
-        
+
         :param description: the value for the short description attribute
         :type description: str or unicode
-    
+
         :return: the decorator function
         :rtype: function
         """
-        def wrap(f):
+        def wrap(func):  # pylint: disable=missing-docstring
             # set description
-            f.short_description = description
+            func.short_description = description
             # add function to admin actions
-            self.append(f)
+            self.append(func)
             # we do not need to modify the function
-            return f
+            return func
         return wrap
 
 
@@ -66,16 +69,18 @@ class NoDeleteSelectedModelAdminMixin(object):
     This mixin removes the delete_selected admin action from the
     admin action drop down, when show_delete_selected is set to False.
     """
-    
+
     show_delete_selected = False
 
     def get_actions(self, request):
+        # pylint: disable=missing-docstring
         actions = super(NoDeleteSelectedModelAdminMixin,
                         self).get_actions(request)
         if not self.get_show_delete_selected():
             if 'delete_selected' in actions:
                 del actions['delete_selected']
         return actions
-    
+
     def get_show_delete_selected(self):
+        # pylint: disable=missing-docstring
         return self.show_delete_selected
--- djhelpers/modelhelpers.py
+++ djhelpers/modelhelpers.py
@@ -1,32 +1,40 @@
 # Copyright 2014 Michael Trunner
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Some django module helpers.
+"""
 
 def short_description(description, **kwargs):
     """
     This decorator adds the django short_description attribute to the
     given function.
-    
+
     It also adds every keyword argument as extra attribute to the
     decorated function.
-    
+
     :param description: the value for the short description attribute
     :type description: str or unicode
-    
+
     :return: the decorator function
-    :rtype: function 
+    :rtype: function
     """
     def _wrapper(func):
+        """
+        Internal wrapper function.
+
+        It only adds some attributes to the function object.
+        """
         func.short_description = description
         for key in kwargs:
             setattr(func, key, kwargs[key])
